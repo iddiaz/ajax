3e3722a09faa8a79a80cf8970c2a84b4e4c974e5
iddiaz <diazdiazivan@gmail.com>
2017-07-02 12:21:20 +0200

    ocntruccion dinamica de arrays apartir de json

--- js/ajax.js
+++ js/ajax.js
@@ -3,12 +3,37 @@
 
 	$.ajax({
 		type: 'GET',
-		url : '',
+		url : 'http://www.json-generator.com/api/json/get/bUjXBzSHKG?indent=2',
 		dataType: 'json'
 	})
 	.done(function( data ){
 		
+		var persons = data;
+
+		console.log(persons);
+
 		
+		for( var i = 0; i< persons.length; i++){
+
+			console.log(persona);
+			var persona = persons[i];
+			var tags = '';
+
+			for(var j=0; j < persona.tags.length; j++){
+				
+				tags +='<span class="label label-primary">'+ persona.tags[j] + '</span> ';
+				console.log('tags>>',tags);
+			}
+
+			var html  = '';
+				html += '<tr>';
+				html += '	<td>'+ persona.name +'</td>';
+				html += '	<td>'+ persona.age +'</td>';
+				html += '	<td>'+ tags +'</td>';
+				html += '</tr>';
+
+				$('.table').append(html)
+		}	
 
 
 
